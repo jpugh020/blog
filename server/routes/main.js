e69607ab1230ae04001c0575a4d3988b9e8368de
JPugh020 <jacobpugh020@gmail.com>
2024-11-24 22:29:18 -0600

    blog project

---
+++ server/routes/main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+//Routes
+router.get('', (req, res) => {
+
+    const locals = {
+        title: "Blog",
+        description: "Simple blog with Node.js, express, and MongoDB"
+    }
+    
+    res.render('index', {locals})
+});
+
+router.get('/about', (req, res) => {
+
+    res.render('about')
+});
+
+module.exports = router;
